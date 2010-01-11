59af50a1a40b65afa3e2fa9a743c620132f832c5
Jonas Baumann <j.baumann@4teamwork.ch>
2010-01-11 14:46:36 +0000

    egg cleanup

--- setup.py
+++ setup.py
@@ -6,34 +6,35 @@ maintainer = 'Thomas Buchberger'
 
 setup(name='ftw.mail',
       version=version,
-      description="Provides a mail content type and a mail-in behavior",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      description="Provides a mail content type and a mail-in behavior" + \
+          ' (Maintainer: %s)' % maintainer,
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         ],
       keywords='',
-      author='Thomas Buchberger',
-      author_email='t.buchberger@4teamwork.ch',
-      url='http://www.4teamwork.ch',
-      license='GPL',
+      author='%s, 4teamwork GmbH',
+      author_email='mailto:info@4teamwork.ch',
+      url='http://psc.4teamwork.ch/4teamwork/ftw/ftw.mail/',
+      license='GPL2',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw'],
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          'BeautifulSoup',
-          'Plone',
-          'plone.app.dexterity',
-          'plone.app.registry',
-          'plone.namedfile',
-          'collective.autopermission',
-          'collective.testcaselayer',
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        'BeautifulSoup',
+        'Plone',
+        'plone.app.dexterity',
+        'plone.app.registry',
+        'plone.namedfile',
+        'collective.autopermission',
+        'collective.testcaselayer',
+        # -*- Extra requirements: -*-
+        ],
       entry_points="""
       # -*- Entry points: -*-
 
