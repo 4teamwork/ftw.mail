ee1225623407f4c264eb4aa7e5104c73a1074e2e
Mathias Leimgruber <m.leimgruber@4teamwork.ch>
2013-06-18 11:38:16 +0200

    Load plone.app.intid profile

--- ftw/mail/tests/layer.py
+++ ftw/mail/tests/layer.py
@@ -3,10 +3,12 @@ import collective.testcaselayer.ptc
 
 ptc.setupPloneSite()
 
+
 class IntegrationTestLayer(collective.testcaselayer.ptc.BasePTCLayer):
 
     def afterSetUp(self):
         # Install the ftw.mail product
+        self.addProfile('plone.app.intid:default')
         self.addProfile('ftw.mail:default')
 
 Layer = IntegrationTestLayer([collective.testcaselayer.ptc.ptc_layer])
