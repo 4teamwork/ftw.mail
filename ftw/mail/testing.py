132fecd7caefa72312ed6fdc01b179cf4872b723
Joni Orponen <j.orponen@4teamwork.ch>
2018-09-17 16:56:31 +0200

    Squelch a layer setup complaint - ftw.mail is not a product.

--- ftw/mail/testing.py
+++ ftw/mail/testing.py
@@ -1,11 +1,11 @@
 from ftw.builder.testing import BUILDER_LAYER
 from ftw.builder.testing import functional_session_factory
 from ftw.builder.testing import set_builder_session_factory
-from ftw.mail.tests import builders
+from ftw.mail.tests import builders  # noqa
+from plone.app.testing import applyProfile
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
-from plone.app.testing import applyProfile
 from plone.testing import z2
 from zope.configuration import xmlconfig
 
@@ -21,23 +21,21 @@ class FtwMailLayer(PloneSandboxLayer):
             '  <includePlugins package="plone" />'
             '  <includePluginsOverrides package="plone" />'
             '</configure>',
-            context=configurationContext)
+            context=configurationContext,
+        )
 
         import Products.CMFPlacefulWorkflow
-        xmlconfig.file('configure.zcml', Products.CMFPlacefulWorkflow,
-                       context=configurationContext)
+        xmlconfig.file('configure.zcml', Products.CMFPlacefulWorkflow, context=configurationContext)
 
         try:
             # Plone >= 4.3.x
             import Products.DataGridField
-            xmlconfig.file('configure.zcml', Products.DataGridField,
-                           context=configurationContext)
+            xmlconfig.file('configure.zcml', Products.DataGridField, context=configurationContext)
         except:
             pass
 
         z2.installProduct(app, 'ftw.file')
         z2.installProduct(app, 'ftw.meeting')
-        z2.installProduct(app, 'ftw.mail')
         z2.installProduct(app, 'ftw.workspace')
 
     def setUpPloneSite(self, portal):
@@ -48,6 +46,6 @@ class FtwMailLayer(PloneSandboxLayer):
 
 FTW_MAIL_LAYER = FtwMailLayer()
 FTW_MAIL_FUNCTIONAL_TESTING = FunctionalTesting(
-    bases=(FTW_MAIL_LAYER,
-           set_builder_session_factory(functional_session_factory)),
-    name="ftw.mail:functional")
+    bases=(FTW_MAIL_LAYER, set_builder_session_factory(functional_session_factory)),
+    name="ftw.mail:functional",
+)
